00736d9984ecc4b3b29c53064056f9f498635881
SharadSavariya3363 <sharad.ownai@gmail.com>
2021-08-27 11:28:44 +0530

    [fix]-(app-routing)>layout routing , layout module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  {path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule)},
   {path: 'login', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)},
   {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
 ]
